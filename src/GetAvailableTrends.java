a999859a422ff5d139aafc9a8a145953ed469ad5
jcpr765 <jcpr626@hotmail.com>
2015-09-20 03:15:10 -0300

    Changes to hashtag receiving, need to work on limiting times this class is run

--- src/GetAvailableTrends.java
+++ src/GetAvailableTrends.java
@@ -9,20 +9,20 @@ import twitter4j.auth.AccessToken;
 public class GetAvailableTrends {
 	public static void main(String[] args) {
 
+		int[] woeid = {23424935, 2459115, 2466256, 2442047, 2379574, 2424766, 44418, 615702};
+		ArrayList<String> hashList = null;
 		Twitter twitter = new TwitterFactory().getInstance();
 
 		// Consumer and Auth Access Token
 		twitter.setOAuthConsumer("ab6ro9XNrx3DPUR9Ly1jF380M", "YGkuhQCm1YrqLpdtSKsP3CkWsdWBEfoUyPCIYk4xvlGwBZKZq8");
 		twitter.setOAuthAccessToken(new AccessToken("3618212414-01XEpSoMdJwTygWEeNL06ehWvo6xy1bfKO5bWs2",
 				"v444cPMjTB6zftnFeqG4uHPxBT7Ia8MdSHJhxzoq9nPyR"));
-		int[] woeid = {23424935,369300,779199,369301,346215,
-				419688,2477171,137884,364497,369302,137883,55894406,
-				91862995,56068648,90270522,90331918} ;
 		try {
+			hashList = new ArrayList<String>();
+			for(int id : woeid){
 			Trends trends;
-			ArrayList<String> hashList = new ArrayList<String>();
 			//for(int i=0; i< woeid.length; i++){
-				trends = twitter.getPlaceTrends(23424935);
+				trends = twitter.getPlaceTrends(id);
 
 				String str = trends.toString();
 				int hashIndex = 0;
@@ -35,9 +35,15 @@ public class GetAvailableTrends {
 					aposIndex = str.indexOf("'", hashIndex++);
 					hashList.add(str.substring(hashIndex+4, str.indexOf("'", aposIndex+1)));
 				}
+			}
 		//	}
-			for(String s : hashList)
+			int count = 0;
+			for(String s : hashList){
+				if(s.charAt(0) == '#')
+					count++;
 				System.out.println(s);
+			}
+			System.out.println(count);
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
