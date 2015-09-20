f74fc525f1e2d1ea5b19349b8e6853075172d10f
jcpr765 <jcpr626@hotmail.com>
2015-09-20 05:21:01 -0300

    File management works now

--- src/GetAvailableTrends.java
+++ src/GetAvailableTrends.java
@@ -12,27 +12,30 @@ import twitter4j.auth.AccessToken;
 
 public class GetAvailableTrends {
 	public static void main(String[] args) throws FileNotFoundException {
-		final long HALF_HR_IN_MS = 1800000;
+		final long FIFTEEN_MIN_IN_MS = 1800000/2;
 		int[] woeid = {23424935, 2459115, 2466256, 2442047, 2379574, 2424766, 44418, 615702};
 		ArrayList<String> hashList = null;
 		File t_file = new File("files/twit_time.txt");
-		PrintWriter pw = new PrintWriter(t_file);
+		File h_file = new File("files/tags_list.txt");
 		Scanner fr = new Scanner(t_file);
-		
-		if(!fr.hasNext() || System.currentTimeMillis() - fr.nextLong() > HALF_HR_IN_MS){
+		if(!fr.hasNext() || System.currentTimeMillis() - fr.nextLong() > FIFTEEN_MIN_IN_MS){
+			PrintWriter time_writer = new PrintWriter(t_file);
+			PrintWriter hash_writer = new PrintWriter(h_file);
 			Twitter twitter = new TwitterFactory().getInstance();
 	
 			// Consumer and Auth Access Token
 			twitter.setOAuthConsumer("ab6ro9XNrx3DPUR9Ly1jF380M", "YGkuhQCm1YrqLpdtSKsP3CkWsdWBEfoUyPCIYk4xvlGwBZKZq8");
 			twitter.setOAuthAccessToken(new AccessToken("3618212414-01XEpSoMdJwTygWEeNL06ehWvo6xy1bfKO5bWs2",
 					"v444cPMjTB6zftnFeqG4uHPxBT7Ia8MdSHJhxzoq9nPyR"));
+			
+			//Overwriting the old file
 			try {
 				hashList = new ArrayList<String>();
 				for(int id : woeid){
 				Trends trends;
 				//for(int i=0; i< woeid.length; i++){
 					trends = twitter.getPlaceTrends(id);
-	
+				//searches for names of trending topics around the world
 					String str = trends.toString();
 					int hashIndex = 0;
 					int aposIndex = 0;
@@ -45,19 +48,30 @@ public class GetAvailableTrends {
 						hashList.add(str.substring(hashIndex+4, str.indexOf("'", aposIndex+1)));
 					}
 				}
+				//Saving time after requests were made to API
+				time_writer.println(System.currentTimeMillis());
 			//	}
+			//Overwriting our hashtag file for our new list of top hashtags
 			int count = 0;
 			for(String s : hashList){
-				if(s.charAt(0) == '#')
+				if(s.charAt(0) == '#'){
+					hash_writer.println(s);
 					count++;
-				System.out.println(s);
+					System.out.println(s);
+				}
 			}
 			System.out.println(count);
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
+			time_writer.close();
+			hash_writer.close();
 		}
+		else{
+			System.out.println("Your last request has been less than 15 minutes ago, cannot follow request yet.");
+		}
+		fr.close();
 	}
 
 }
