65edf4c346962f7faf6ffbd9e9dbdf63b0765d67
jcpr765 <jcpr626@hotmail.com>
2015-09-20 04:28:26 -0300

    adding code to work with created file. Still needs work

--- src/GetAvailableTrends.java
+++ src/GetAvailableTrends.java
@@ -3,6 +3,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import twitter4j.Trends;
 import twitter4j.Twitter;
@@ -11,37 +12,40 @@ import twitter4j.auth.AccessToken;
 
 public class GetAvailableTrends {
 	public static void main(String[] args) throws FileNotFoundException {
-//yo
+		final long HALF_HR_IN_MS = 1800000;
 		int[] woeid = {23424935, 2459115, 2466256, 2442047, 2379574, 2424766, 44418, 615702};
 		ArrayList<String> hashList = null;
 		File t_file = new File("files/twit_time.txt");
 		PrintWriter pw = new PrintWriter(t_file);
-		Twitter twitter = new TwitterFactory().getInstance();
-
-		// Consumer and Auth Access Token
-		twitter.setOAuthConsumer("ab6ro9XNrx3DPUR9Ly1jF380M", "YGkuhQCm1YrqLpdtSKsP3CkWsdWBEfoUyPCIYk4xvlGwBZKZq8");
-		twitter.setOAuthAccessToken(new AccessToken("3618212414-01XEpSoMdJwTygWEeNL06ehWvo6xy1bfKO5bWs2",
-				"v444cPMjTB6zftnFeqG4uHPxBT7Ia8MdSHJhxzoq9nPyR"));
-		try {
-			hashList = new ArrayList<String>();
-			for(int id : woeid){
-			Trends trends;
-			//for(int i=0; i< woeid.length; i++){
-				trends = twitter.getPlaceTrends(id);
-
-				String str = trends.toString();
-				int hashIndex = 0;
-				int aposIndex = 0;
-				while (true) {
-					hashIndex = str.indexOf("name=", hashIndex);
-					if (hashIndex == -1)
-						break;
-					hashIndex++;
-					aposIndex = str.indexOf("'", hashIndex++);
-					hashList.add(str.substring(hashIndex+4, str.indexOf("'", aposIndex+1)));
+		Scanner fr = new Scanner(t_file);
+		
+		if(!fr.hasNext() || System.currentTimeMillis() - fr.nextLong() > HALF_HR_IN_MS){
+			Twitter twitter = new TwitterFactory().getInstance();
+	
+			// Consumer and Auth Access Token
+			twitter.setOAuthConsumer("ab6ro9XNrx3DPUR9Ly1jF380M", "YGkuhQCm1YrqLpdtSKsP3CkWsdWBEfoUyPCIYk4xvlGwBZKZq8");
+			twitter.setOAuthAccessToken(new AccessToken("3618212414-01XEpSoMdJwTygWEeNL06ehWvo6xy1bfKO5bWs2",
+					"v444cPMjTB6zftnFeqG4uHPxBT7Ia8MdSHJhxzoq9nPyR"));
+			try {
+				hashList = new ArrayList<String>();
+				for(int id : woeid){
+				Trends trends;
+				//for(int i=0; i< woeid.length; i++){
+					trends = twitter.getPlaceTrends(id);
+	
+					String str = trends.toString();
+					int hashIndex = 0;
+					int aposIndex = 0;
+					while (true) {
+						hashIndex = str.indexOf("name=", hashIndex);
+						if (hashIndex == -1)
+							break;
+						hashIndex++;
+						aposIndex = str.indexOf("'", hashIndex++);
+						hashList.add(str.substring(hashIndex+4, str.indexOf("'", aposIndex+1)));
+					}
 				}
-			}
-		//	}
+			//	}
 			int count = 0;
 			for(String s : hashList){
 				if(s.charAt(0) == '#')
@@ -52,6 +56,7 @@ public class GetAvailableTrends {
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
+		}
 		}
 	}
 
