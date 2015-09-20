7f4af25f705e7ddc68d2e28f7cd18637ce7c82e3
juanjalvarez <jalvarez5241997@gmail.com>
2015-09-19 22:51:14 -0400

    Experimental hashtag object implementation

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+import java.awt.Font;
 import java.util.ArrayList;
 
 import org.newdawn.slick.AppGameContainer;
@@ -6,15 +7,18 @@ import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.TrueTypeFont;
+
 //andy is poopyhead
 public class Game extends BasicGame {
-	
+
 	public static final int SCREEN_WIDTH = 1280;
 	public static final int SCREEN_HEIGHT = 720;
-	
+
 	private ArrayList<HashtagObject> hashList;
-	float x;
-	float size;
+	private float x;
+	private float size;
+	private TrueTypeFont font;
 
 	public Game(String title) {
 		super(title);
@@ -23,26 +27,42 @@ public class Game extends BasicGame {
 	@Override
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
 		arg1.drawRect(20, x, size, size);
+		for (HashtagObject obj : hashList){
+			arg1.setFont(font);
+			arg1.drawRect(obj.getX(), obj.getY(), obj.getHitbox().getWidth(), obj.getHitbox().getHeight());
+			arg1.drawString(obj.getText(), obj.getX(), obj.getY());
+		}
 	}
 
 	@Override
 	public void init(GameContainer arg0) throws SlickException {
 		hashList = new ArrayList<HashtagObject>();
-		x=0;
+		x = 0;
 		size = 10;
+		hashList.add(new HashtagObject(this, "potato", 5, 32));
+		hashList.add(new HashtagObject(this, "tomato", 5, 32));
+		hashList.add(new HashtagObject(this, "ching", 5, 32));
+		hashList.add(new HashtagObject(this, "chong", 5, 32));
+		Font f = new Font("Verdana", Font.BOLD, 32);
+		font = new TrueTypeFont(f, false);
 	}
 
 	@Override
 	public void update(GameContainer arg0, int arg1) throws SlickException {
 		Input i = arg0.getInput();
-		if(i.isKeyDown(Input.KEY_UP))
-			x-=5;
-		if(i.isKeyDown(Input.KEY_DOWN))
-			x+=5;
-		if(x+(size/2) > SCREEN_HEIGHT)
+		if (i.isKeyDown(Input.KEY_UP))
+			x -= 5;
+		if (i.isKeyDown(Input.KEY_DOWN))
+			x += 5;
+		if (x + (size / 2) > SCREEN_HEIGHT)
 			x = SCREEN_HEIGHT;
-		if(x-(size/2) < 0)
+		if (x - (size / 2) < 0)
 			x = 0;
+		for (int x = 0; x < hashList.size(); x++) {
+			hashList.get(x).move();
+			if (!hashList.get(x).isAlive())
+				hashList.remove(x);
+		}
 	}
 
 	public static void main(String[] args) throws SlickException {
--- src/HashtagObject.java
+++ src/HashtagObject.java
@@ -1,57 +1,75 @@
+import java.util.Random;
+
+import org.newdawn.slick.geom.Rectangle;
 
 public class HashtagObject {
-	
+
 	private String text;
 	private float x;
 	private float y;
 	private float speed;
 	private float size;
 	private Game game;
-	
-	public HashtagObject(Game game, String text, float speed, float size){
+
+	public HashtagObject(Game game, String text, float speed, float size) {
 		this.game = game;
 		this.text = text;
 		this.speed = speed;
 		this.size = size;
+		x = Game.SCREEN_WIDTH;
+		y = new Random().nextInt(Game.SCREEN_HEIGHT);
 	}
-	
+
 	public String getText() {
 		return text;
 	}
-	
+
 	public void setText(String text) {
 		this.text = text;
 	}
-	
+
 	public float getX() {
 		return x;
 	}
-	
+
 	public void setX(float x) {
 		this.x = x;
 	}
-	
+
 	public float getY() {
 		return y;
 	}
-	
+
 	public void setY(float y) {
 		this.y = y;
 	}
-	
+
 	public float getSpeed() {
 		return speed;
 	}
-	
+
 	public void setSpeed(float speed) {
 		this.speed = speed;
 	}
-	
+
 	public float getSize() {
 		return size;
 	}
-	
+
 	public void setSize(float size) {
 		this.size = size;
+	}
+
+	public Rectangle getHitbox() {
+		return new Rectangle(x, y, size * text.length(), size);
+	}
+
+	public void move() {
+		x -= speed;
+	}
+
+	public boolean isAlive() {
+		return true;
+		//return (x + size) < 0;
 	}
 }
